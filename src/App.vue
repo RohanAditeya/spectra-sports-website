8b979d72797d4cd83cfb247337ba2bd45ab69605
rohan-aditeya <rohan.aditeya@gmail.com>
2025-11-18 22:32:01 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import HomePage from './pages/HomePage.vue';
+</script>
+
+<template>
+  <HomePage/>
+</template>
+
+<style>
+  :root {
+    --header-height: 12vh
+  }
+  body {
+    background-color: #f3f5fa;
+    padding: 0px;
+    margin: 0px;
+  }
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+  import { Icon } from '@iconify/vue'
+  import logoUrl from '../static/spectra-logo.svg'
+</script>
+
+<template>
+  <header>
+    <div :class="['navbar']">
+      <img :class="['logo-img']" :src="logoUrl" alt="Spectra Sports Logo"/>
+      <div :class="['menu']">
+        <Icon icon="radix-icons:hamburger-menu" :class="['menu-icon']"/>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="css" scoped>
+  header {
+    max-height: var(--header-height);
+    border-bottom-color: #ee334e;
+    border-bottom-width: thin;
+    border-bottom-style: solid;
+  }
+  .navbar {
+    display: flex;
+    height: var(--header-height);
+    justify-content: space-between;
+  }
+  .logo-img {
+    max-height: 110%;
+    flex: 1;
+  }
+  .menu {
+    display: flex;
+    justify-content: flex-end;
+    padding: 15px;
+    align-items: center;
+    flex: 1;
+  }
+  .menu-icon {
+    font-size: 2em;
+  }
+</style>
